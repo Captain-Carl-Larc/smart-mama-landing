1fc0dcd3bdd4858c3a098e9747823880500ec551
Stephen Kalwenzi <stephenmutwiiofficial@gmail.com>
2025-06-25 13:22:33 +0300

    styled banner

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,7 +1,7 @@
 function Banner({ title, description, buttonText }) {
   return (
     <div className="call-to-action bg-gray-800 p-8 mt-12 rounded-lg shadow-lg">
-      <h3 className="text-2xl text-white font-semibold text-center mt-8 mb-4">
+      <h3 className="text-2xl text-white font-bold text-center mt-8 mb-4">
         {title || "Join Our Community Today!"}
       </h3>
       <p className="text-center text-white mb-6">
@@ -9,7 +9,7 @@ function Banner({ title, description, buttonText }) {
           "Sign up now to start sharing your videos and connecting with others!"}
       </p>
       <div className="flex justify-center">
-        <button className="bg-blue-500 text-white px-6 py-3 rounded hover:bg-blue-600 transition duration-300">
+        <button className="bg-blue-500 text-white px-6 py-3 font-bold rounded hover:bg-blue-600 transition duration-300">
           {buttonText || "Sign Up Now"}
         </button>
       </div>
