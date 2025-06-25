dea30deebf62bf50c1a206aff26d96f4c3770b8c
Stephen Kalwenzi <stephenmutwiiofficial@gmail.com>
2025-06-25 11:50:23 +0300

    colors for visibility

--- src/components/benefits.jsx
+++ src/components/benefits.jsx
@@ -3,7 +3,9 @@ function Benefits() {
     <section className="bg-gray-100 py-8 p-4 md:p-8">
       <div className="benefit-1 grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
         <div className="benefit-text-container">
-          <h2 className="text-3xl font-bold mb-4">Benefit One</h2>
+          <h2 className="text-3xl font-bold mb-4  text-gray-900">
+            Benefit One
+          </h2>
           <p className="text-gray-700 mb-4">
             This is a brief description of the first benefit. It highlights the
             key features and advantages that users can expect.
@@ -13,7 +15,7 @@ function Benefits() {
             numquam sunt, officia nostrum optio veritatis molestias totam nam
             est a quasi at maxime. Optio, saepe.
           </p>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-300">
+          <button className="bg-blue-500  text-gray-900 px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-300">
             Learn More
           </button>
         </div>
@@ -34,12 +36,14 @@ function Benefits() {
           />
         </div>
         <div className="benefit-text-container md:order-1">
-          <h2 className="text-3xl font-bold mb-4">Benefit Two</h2>
+          <h2 className="text-3xl font-bold mb-4  text-gray-900">
+            Benefit Two
+          </h2>
           <p className="text-gray-700 mb-4">
             This is a brief description of the second benefit. It emphasizes the
             unique selling points and how it can help users.
           </p>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-300">
+          <button className="bg-blue-500  text-gray-900 px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-300">
             Learn More
           </button>
         </div>
