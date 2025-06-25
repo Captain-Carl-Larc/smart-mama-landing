8d2fbe809e7c46ecf2f4ca1d04c17fbe7bb49d84
Stephen Kalwenzi <stephenmutwiiofficial@gmail.com>
2025-06-25 09:02:20 +0300

    made navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <nav className="bg-white shadow-md fixed w-full z-10 top-0 left-0 p-4">
+            <div className="logo">
+                <h2 className="text-xl md:text-3xl font-bold text-blue-800">
+                    SmartMama
+                </h2>
+            </div>
+        </nav>
+    )
+}
+    
+
+export default Navbar;
