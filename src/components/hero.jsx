eabab2c197563cf85036694d366d940c1e2578f8
Stephen Kalwenzi <stephenmutwiiofficial@gmail.com>
2025-06-25 12:14:09 +0300

    copyrighted hero section

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -6,18 +6,17 @@ function Hero() {
     <section className="pt-20 md:pt-32 grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 gap-2 md:gap-4 p-4 md:p-8 bg-gray-100">
       <div id="text-container">
         <h1 className="text-4xl font-bold mb-4  text-gray-900">
-          Welcome to Our Website
+          Your <span className="text-blue-800">Confident</span> Pregnancy
+          Journey Begins Here.
         </h1>
         <p className="text-lg mb-6 text-gray-900">
-          Discover amazing content and connect with our community. We are
-          dedicated to providing you with the best experience possible.
+          SmartMama provides personalized guidance and expert support to
+          optimize your health, prepare confidently for birth, and embrace
+          motherhood with peace of mind.
         </p>
         <div className="buttons-container flex space-between justify-center items-center md:justify-start">
           <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">
-            Sign Up
-          </button>
-          <button className="bg-gray-300 text-gray-800 px-4 py-2 rounded hover:bg-gray-400 transition duration-300 ml-4">
-            Log In
+            Discover Your Support
           </button>
         </div>
       </div>
